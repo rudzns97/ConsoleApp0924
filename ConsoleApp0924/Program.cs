ba975710d44171be21723f3ff67a7bdec1d379d0
염경진 <rudzns97@naver.com>
2020-09-24 17:11:05 +0900

    초기 커밋.

---
+++ ConsoleApp0924/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp0924
+{
+    //예외처리
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                BBB();
+                string eee = Console.ReadLine();
+            }
+            catch(Exception err)
+            {
+                Console.WriteLine(err.Message);
+            }
+        }
+        private static void BBB()
+        {
+            AAA();
+        }
+        static void AAA()
+        {
+
+                int[] arr = new int[5] { 1, 2, 0, 4, 5 };
+                Console.WriteLine("몇번째 배열의 값으로 계산할까요?");
+                int idx = int.Parse(Console.ReadLine());
+
+                if (idx < 0 || idx > 4)
+                {
+                    //Console.WriteLine("0~4 범위안의 수를 입력하세요.");
+                    throw new Exception("0~4 범위안의 수를 입력하세요.");
+                }
+
+                int p = arr[idx];
+                int k = 10 / p;
+
+                Console.WriteLine(k);
+            }
+            //catch (DivideByZeroException)
+            //{
+            //    Console.WriteLine("0으로 나눌 수 없습니다.");
+            //}
+            //catch (IndexOutOfRangeException)
+            //{
+            //    Console.WriteLine("0~4 범위안의 수를 입력하세요.");
+            //}
+            //catch (Exception err)
+            //{
+            //    //Console.WriteLine("처리중 오류가 발생했습니다. 다시한번 시도");
+            //    throw err;
+            //}
+            
+        }
+    }
+
---
+++ ConsoleApp0924/SingleTon1.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Dynamic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp0924
+{
+   
+    class SingleTon1
+    {
+        static SingleTon1 instance;
+        private SingleTon1()
+        {
+
+        }
+
+        public static SingleTon1 CreateInstance()
+        {
+            
+            if (instance == null)
+                instance = new SingleTon1();
+
+            return instance;
+        }
+        public void Method()
+        {
+            Console.WriteLine("메서드 실행");
+        }
+    }
+    class Test
+    {
+        static void Main()
+        {
+            SingleTon1 c1 = SingleTon1.CreateInstance();
+            c1.Method();
+
+            SingleTon1 c2 = SingleTon1.CreateInstance();
+            c2.Method();
+
+            c2 = SingleTon1.CreateInstance();
+            c2.Method();
+        }
+    }
+}
